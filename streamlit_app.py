48c1e0c5903c5dfb118367d52290b576444ecb2f
Prodduturi5 <prodduturisathwik5@gmail.com>
2025-02-19 10:30:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-   "In this app create the  IT b section , Today date 2/19/2025 "
-    " performed by the sathwik"
+   "This App Is Created By Sathwik"
 )
